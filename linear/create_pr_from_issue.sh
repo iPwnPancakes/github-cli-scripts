bdbf5a49c28245d20e0a035acc23dce12e542993
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2023-12-27 15:21:54 -0600

    No longer depending on hard-coded gh cli command

--- linear/create_pr_from_issue.sh
+++ linear/create_pr_from_issue.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 branch_name=$(git branch --show-current)
 
-issue_title=$(gh lin-s $branch_name)
+issue_title=$($SCRIPT_DIR/retrieve_issue.sh $branch_name)
 issue_title_exit_code=$?
 
 if [ $issue_title_exit_code -eq 1 ]; then
